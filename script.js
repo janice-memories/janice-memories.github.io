d041fae06d7d2334814a13fce9e19a871d01369d
janice-memories <wisely2113@gmail.com>
2024-09-11 05:47:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById('messageButton').addEventListener('click', function() {
     document.getElementById('message').style.display = 'block';
+    const audio = document.getElementById('birthday-audio');
+    audio.play();
 });
